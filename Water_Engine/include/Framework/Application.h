0277080037a311c365011873bffd3f60cca9a617
WillTheWater <willthewater@protonmail.com>
2025-10-26 15:08:20 +0100

    Setup Tick and FPS

--- Water_Engine/include/Framework/Application.h
+++ Water_Engine/include/Framework/Application.h
@@ -11,6 +11,13 @@ namespace we
 		void Run();
 
 	private:
+		void Tick(float DeltaTime);
+		void Render();
+
 		sf::RenderWindow Window;
+
+		float TargetFramerate;
+
+		sf::Clock TickClock;
 	};
 }
--- Water_Engine/src/Framework/Application.cpp
+++ Water_Engine/src/Framework/Application.cpp
@@ -1,14 +1,22 @@
 #include "Framework/Application.h"
+#include <iostream>
 
 namespace we
 {
 	Application::Application()
-		: Window{ sf::VideoMode({ 1920, 1080 }), "Game Window" }
+		: Window{ sf::VideoMode({ 1920, 1080 }), "Game Window" },
+		TargetFramerate{60.f},
+		TickClock{}
 	{
 		
 	}
+
 	void Application::Run()
 	{
+		TickClock.restart();
+		float AccumulatedTime = 0.f;
+		float TargetDeltaTime = 1.f / TargetFramerate;
+
         while (Window.isOpen())
         {
             while (const std::optional event = Window.pollEvent())
@@ -16,9 +24,20 @@ namespace we
                 if (event->is<sf::Event::Closed>())
                     Window.close();
             }
-
-            Window.clear();
-            Window.display();
         }
+		AccumulatedTime += TickClock.restart().asSeconds();
+		while (AccumulatedTime > TargetDeltaTime)
+		{
+			AccumulatedTime -= TargetDeltaTime;
+			Tick(TargetDeltaTime);
+			Render();
+		}
+	}
+	void Application::Tick(float DeltaTime)
+	{
+		std::cout << "FPS: " << 1.f / DeltaTime << std::endl;
+	}
+	void Application::Render()
+	{
 	}
 }
