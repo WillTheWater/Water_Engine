749f5efd58a2f5c2ac74d21e8996883c6b55a00c
WillTheWater <willthewater@protonmail.com>
2025-12-25 19:30:28 +0100

    Water Engine v1.0.0

--- TemplateGame/include/config.h
+++ TemplateGame/include/config.h
@@ -7,6 +7,6 @@ std::string GetAssetDirectory()
 #ifdef  NDEBUG
 	return "assets/";
 #else
-	return "C:/Dev/Water_Engine/WaterGame/assets/";
+	return "C:/Dev/Water_Engine/TemplateGame/assets/";
 #endif
 }
